12f573bd94d7df6d33232e6db445afd667c4565a
PJ Neethling <neethlingpeterjohn@gmail.com>
2022-11-18 13:53:43 +0200

    Add initial Client

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'http://localhost:9998/api' // when you release your app to production, this should change to whatever host or address your API will be deployed to
+};
